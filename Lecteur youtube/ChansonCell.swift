f8d30bbf42023cc8b39ac2624187f1a1f6b28b02
voicedhealer <vivien.bernardot@gmail.com>
2019-01-03 12:06:20 +0100

    Télécharger des miniatures depuis internet

--- Lecteur youtube/ChansonCell.swift
+++ Lecteur youtube/ChansonCell.swift
@@ -27,13 +27,29 @@ class ChansonCell: UITableViewCell {
     }
     func creerCell(_ chanson: Chanson) {
         self.chanson = chanson
-        
+        telechargerImage()
         let attributed = NSMutableAttributedString(string: self.chanson.titre, attributes: [.font: UIFont.boldSystemFont(ofSize: 20), .foregroundColor: UIColor.black])
         let secondeLigne = NSAttributedString(string: "\n" + self.chanson.artiste, attributes: [.font: UIFont.italicSystemFont(ofSize: 20), .foregroundColor: UIColor.darkGray])
         attributed.append(secondeLigne)
         artisteETTitreLabel.attributedText = attributed
     }
-    
+    func telechargerImage() {
+        miniature.image = #imageLiteral(resourceName: "logo")
+        
+        if let url = URL(string: self.chanson.miniatureUrl) {
+            let session = URLSession.shared
+            let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let imageData = data, let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        self.miniature.image = image
+                    }
+                }
+            })
+            
+            task.resume()
+        }
+        
+    }
     
     
 }
--- Lecteur youtube/TableauController.swift
+++ Lecteur youtube/TableauController.swift
@@ -21,6 +21,7 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
         tableView.delegate = self
         tableView.dataSource = self
         ajouterChanson()
+        title = "Mes vidéos préférées"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,33 +40,30 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 170
+        return 130
+        
     }
-    
-    
-    
-    
     func ajouterChanson() {
     chansons = [Chanson]()
-        let cash = Chanson(artiste: "Cordux et Voice", titre: "Cash'n Guns", code: "byVoymKz4E4")
+        let cash = Chanson(artiste: "Cordux et Voice", titre: "Cash'n Guns", code: "byVoymKz4E4&t")
         chansons.append(cash)
-        let timeLine = Chanson(artiste: "Cordux et Voice", titre: "TimeLine", code: "jc7I--fjs0E")
+        let timeLine = Chanson(artiste: "Cordux et Voice", titre: "TimeLine", code: "jc7I--fjs0E&t")
         chansons.append(timeLine)
-        let seenot = Chanson(artiste: "Cordux et Voice", titre: "Seenot", code: "J3MgSInlTmo")
+        let seenot = Chanson(artiste: "Cordux et Voice", titre: "Seenot", code: "J3MgSInlTmo&t")
         chansons.append(seenot)
-        let top5Noel = Chanson(artiste: "Cordux et Voice", titre: "Top 05 Noël 2017", code:"OM6fef8poS8")
+        let top5Noel = Chanson(artiste: "Cordux et Voice", titre: "Top 05 Noël 2017", code:"OM6fef8poS8&t")
         chansons.append(top5Noel)
-        let nosferatu = Chanson(artiste: "Cordux et Voice", titre: "Nosferatu", code: "dbXJVNTbREs")
+        let nosferatu = Chanson(artiste: "Cordux et Voice", titre: "Nosferatu", code: "dbXJVNTbREs&t")
         chansons.append(nosferatu)
-        let colt = Chanson(artiste: "Cordux et Voice", titre: "Colt Express", code: "kS9z9Vv45oY")
+        let colt = Chanson(artiste: "Cordux et Voice", titre: "Colt Express", code: "kS9z9Vv45oY&t")
         chansons.append(colt)
-        let timeStories = Chanson(artiste: "Cordux et Voice", titre: "Time Stories", code: "YdiuqVmbr_E")
+        let timeStories = Chanson(artiste: "Cordux et Voice", titre: "Time Stories", code: "YdiuqVmbr_E&t")
         chansons.append(timeStories)
-        let room25 = Chanson(artiste: "Cordux et Voice", titre: "Room 25", code: "290UHCBSkj0")
+        let room25 = Chanson(artiste: "Cordux et Voice", titre: "Room 25", code: "290UHCBSkj0&t")
         chansons.append(room25)
-        let hanabi = Chanson(artiste: "Cordux et Voice", titre: "Hanabi", code: "vE7_HCfTgQU")
+        let hanabi = Chanson(artiste: "Cordux et Voice", titre: "Hanabi", code: "vE7_HCfTgQU&t")
         chansons.append(hanabi)
-        let meeple = Chanson(artiste: "Cordux et Voice", titre: "Meeple Circus", code: "jq7ZjJH27ms")
+        let meeple = Chanson(artiste: "Cordux et Voice", titre: "Meeple Circus", code: "jq7ZjJH27ms&t")
         chansons.append(meeple)
         
         //Reloadata rechargement de la vidéo dans le tableau de vue mettre à jour
